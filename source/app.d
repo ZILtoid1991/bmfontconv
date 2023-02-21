eb133deb947222bfa9600393da53101ee47df43f
ZILtoid1991 <laszloszeremi@outlook.com>
2023-02-21 23:20:35 +0100

    Work on implementation

--- source/app.d
+++ source/app.d
@@ -6,10 +6,14 @@ static struct CMDargs {
 	@Arg("The source file to be converted", 's', Optional.no) string source;
 	@Arg("Texture size in both direction, default is 256", 't') int texSize;
 	@Arg("Output file name, otherwise matching filename will be used.", 'o') string output;
+	@Arg("Output file type, default is binary.", 'p') string outputType;
 	@Arg("File format for the textures, default is TGA.", 'f') string fFormat;
-	@Arg("Graphical format of the textures, default is 8 bit grayscale.", g) string gFormat;
+	@Arg("Graphical format of the textures, default is 8 bit grayscale.", 'g') string gFormat;
 }
 void main(string[] args) {
 	writeln("BMFontconv by Laszlo Szeremi.");
 	writeln("Converts old bitmap fonts into the newer AngelCode BMFont format.");
+	CMDargs opts;
+	parseArgs(opts, args);
+
 }
--- source/bdf.d
+++ source/bdf.d
@@ -3,7 +3,7 @@ module bdf;
 import rectpack;
 import types;
 
-import bmfont;
+static import bmfont;
 import dimage;
 
 import std.stdio;
@@ -126,6 +126,7 @@ public class BDFParser {
 						case "STARTCHAR":
 							currChar = CharInfo.init;
 							currRect = Rect.init;
+							currRect.id = cast(int)chars.length;
 							mode = Mode.CharParse;
 							break;
 						default:
@@ -135,7 +136,78 @@ public class BDFParser {
 			}
 		}
 	}
-	void convert(string name, int tW, int tH, FormatFlags formatflags) {
-
+	void convert(string name, int tSize, FormatFlags formatflags) {
+		int pageNum, result;
+		Rect[] getUnpackedRects(Rect[] src, ref Rect[] output) {
+			Rect[] result;
+			foreach (Rect key; src) {
+				if (!key.was_packed) {
+					key.pageNum = pageNum;
+					result ~= key;
+				} else {
+					output ~= key;
+				}
+			}
+			return result;
+		}
+		stbrp_context rectPackContext;
+		stbrp_node[] nodes;
+		nodes.length = tSize * 2;
+		Rect[] packedRects, unpackedRects = rects;
+		do {
+			stbrp_init_target(&rectPackContext, tSize, tSize, nodes.ptr, cast(int)nodes.length);
+			result = packRects(&context, unpackedRects);
+			pageNum++;
+			unpackedRects = getUnpackedRects(unpackedRects, packedRects);
+		} while (!result);
+		bmfont.Font outputFont;
+		outputFont.info = bmfont.Font.Info(cast(short)size, 0, 0, 100, 1, [0,0,0,0], [0,0], name, 0);
+		ubyte[][] pages;
+		pages.length = pageNum;
+		foreach (ubyte[] key; pages) {
+			key.length = tSize * tSize;
+		}
+		void writeToPage(int x, int y, int p, ubyte v) {
+			pages[p][x + (y * tSize)] = v;
+		}
+		foreach (Rect key ; packedRects) {
+			CharInfo currCh = chars[key.id];
+			outputFont.chars ~= bmfont.Font.Char(currCh.charID, cast(ushort)key.x, cast(ushort)key.y, cast(ushort)key.w, 
+					cast(ushort)key.h, cast(short)currCh.xOffset, cast(short)currCh.yOffset, cast(short)currCh.xadvance, 
+					cast(ubyte)key.page, bmfont.Channels.all);
+			int i;
+			for (int y ; y < key.h ; y++) {
+				for (int x ; x < key.w ; x++) {
+					writeToPage(x, y, key.page, currCh.bin[i]);
+					i++;
+				}
+			}
+		}
+		IImageData[] finishedPages;
+		if (formatflags.TO_Channels_Mono) {
+			foreach (page ; pages) {
+				finishedPages ~= new MonochromeImageData!ubyte(page, tSize, tSize, PixelFormat.Grayscale8Bit, 8);
+			}
+		}
+		if (formatflags.TextureOut_Targa) {
+			foreach (size_t i, IImageData finishedPage ; finishedPages) {
+				TGA outputImg = new TGA(finishedPage);
+				string filename = name ~ to!string(i) ~ ".tga";
+				File outputFile = File(filename, "wb");
+				outputImg.save(outputFile);
+				outputFont.pages ~= filename;
+			}
+		} else if (formatflags.TextureOut_PNG) {
+			foreach (size_t i, IImageData finishedPage ; finishedPages) {
+				PNG outputImg = new PNG(finishedPage);
+				string filename = name ~ to!string(i) ~ ".png";
+				File outputFile = File(filename, "wb");
+				outputImg.save(outputFile);
+				outputFont.pages ~= filename;
+			}
+		}
+		ubyte[] fontBin = outputFont.toBinary;
+		File outputFile = File(filename ~ ".fnt", "wb");
+		outputFile.rawWrite(fontBin);
 	}
 }
